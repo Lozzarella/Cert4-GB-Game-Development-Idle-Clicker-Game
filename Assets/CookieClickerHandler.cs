25df18646bc67219b7c271b8dbfba2ec47066737
Lozzarella <laurenbrooks70@gmail.com>
2022-03-16 13:31:48 +1100

    Clicker UI

---
+++ Assets/CookieClickerHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieClickerHandler : MonoBehaviour
+{
+    public void Click(string message)
+    {
+        Debug.Log("Clicked Button");
+    }
+    
+}
